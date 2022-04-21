450ab8754aac659411aa5257a20d7983025056ac
Isabelle Palmdahl <isabellepalmdahl@hotmail.com>
2022-04-21 14:11:16 +0100

    Add project files.

---
+++ Eazy.Tours/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Eazy.Tours.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
